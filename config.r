8fda39286b89bc97f688267e0b99914fed8c3dc9
Maya Luna <maya@progresslibs.org>
2025-07-13 18:13:54 -0400

    wowza

--- config.r
+++ config.r
@@ -1,3 +1,5 @@
+monthly_change <- new.env()
+
 CONFIG <- list(
     activity_data_path = "G:/My Drive/activitytracker/stt_records_automatic.csv",
     default_start_date = "2024-01-22",
--- data_munch.r
+++ data_munch.r
@@ -27,3 +27,90 @@ activities.formatted <- activities_raw %>%
         activity, category, date, duration_minutes, duration,
         time_started, time_ended, hour, month, year, month_year
 )
+
+# --- formatting function ---
+formatting <- function(df, x) {
+    df %>% 
+    group_by(month_year, !!sym(x)) %>%  
+    summarise(
+        total_minutes = sum(duration, na.rm = TRUE),
+        total_hours = sum(duration/60, na.rm = TRUE),
+        .groups='drop'
+    ) %>%
+    select(!!sym(x), month_year, total_minutes, total_hours) 
+}
+
+#filtering function. filters to specified date range
+filter_time <- function(df, start_date, end_date) {
+    df %>%
+    filter(between(month_year, as.Date(start_date), as.Date(end_date)))%>%
+    arrange(month_year)
+}
+
+# --- monthly.change function ---
+monthly.change <- function(df,x) {
+    df %>%
+        filter(
+            if (identical(df, mon.act.tot.hrs)) {
+                !!sym("activity") == x  
+            } else {
+                !!sym("category") == x 
+            }
+        ) %>%
+        arrange(month_year) %>%
+        mutate(
+            change = total_hours - lag(total_hours),
+            percent_change = (change/lag(total_hours)) * 100
+        ) %>%
+        select(month_year, total_hours, change, percent_change)
+}
+
+#outputs all MoM changes for each sub-attribute of x. 
+all.changes <- function(x) {
+    if (x == "category") {
+        df <- mon.cat.tot.hrs
+    } else {
+        df <- mon.act.tot.hrs
+    }
+
+    df_transform <- df %>%
+        group_by(!!sym(x)) %>%
+        arrange(!!sym(x), month_year) %>% 
+        mutate(
+            MoM_change = total_hours - lag(total_hours),
+            percent_change = (MoM_change / lag(total_hours)) * 100,
+            summary = sprintf("%.1f hrs, %+.1f%%",
+                total_hours, percent_change)
+        ) %>%
+        ungroup() %>%
+        select(month_year, !!sym(x), total_minutes, MoM_change, percent_change, summary)  # FIXED
+    split_dfs <- split(df_transform, df_transform[[x]])
+    return(split_dfs)
+}
+
+
+#Creates an MoM change df for each category and activity
+mon_change_all <- function(c) {
+    df_list <- all.changes(c)
+    clean_names <- names(df_list) %>%
+        str_replace_all(" ", "_") %>%
+        tolower()
+    named_list <- setNames(
+        df_list,
+        paste0(clean_names)
+    )
+    # turns each item in named_list into a global variable (df)
+    list2env(named_list, envir = monthly_change)
+    return(named_list)
+}
+
+#list of each activity grouped by hours per day
+mon.act.tot.hrs <- formatting(activities.formatted, "activity")
+
+#list of each category grouped by hours per day
+mon.cat.tot.hrs <- formatting(activities.formatted, "category")
+
+#calls mon_change_all function, creates a MoM change df for each category and activity
+mon_change_all("category")
+
+mon_change_all("activity")
